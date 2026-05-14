bd80f96e5759e47c48e7351a2e26e7429f14dca3
Monika Tomanek <monica.tomanek@gmail.com>
2026-05-14 20:12:12 +0100

    Add Spring Boot Java app

---
+++ java-app/src/main/java/com/adhdhobby/hobbycollector/HobbycollectorApplication.java
@@ -0,0 +1,13 @@
+package com.adhdhobby.hobbycollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbycollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbycollectorApplication.class, args);
+	}
+
+}
